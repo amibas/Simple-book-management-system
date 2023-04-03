eeb0f62ac5f906c48b2d8c322392d734ebfd3a52
amibas <towsod@outlook.com>
2023-04-03 20:51:13 +0800

    数据操作

--- DataManagement.h
+++ DataManagement.h
@@ -6,41 +6,58 @@
 #define SIMPLE_BOOK_MANAGEMENT_SYSTEM_DATAMANAGEMENT_H
 
 
-#define USER_FILE "user.txt"
-#define BOOK_FILE "book.txt"
-#define NOTICE_FILE "notice.txt"
-
-#include "FileTools.h"
+#include "stdio.h"
 #include "stdlib.h"
-
-typedef struct bookInfo {
-    int bookId;             //编号
-    char bookName[20];      //书名
-    char author[10];        //作者
-    int total;              //数量
-    struct bookInfo *next;  //链表链接
-} BOOK;
-
-typedef struct userInfo {
-    int userId;             //用户ID
-    char userName[10];      //用户名
-    char userPassword[16];  //用户密码
-    char sex;               //性别
-    int level;              //权限等级
-    BOOK *bookHead;         //借阅的书籍
-    struct userInfo *next;  //链表链接
-} USER;
-
-typedef struct notice {
-    int noticeId;           //公告ID
-    char noticeTitle[20];   //公告标题
-    char text[400];         //正文
-    char noticeTime[11];    //发布时间
-    struct notice *next;    //链表链接
-} NOTICE;
+#include "string.h"
 
 
-extern int addUser(USER *user);
-extern int findUser(char *userName);    //查找用户
-extern int userPasswordCheck(char *userName, char *userPassword);    //密码检查
+typedef struct book {
+    char book_num[10];                //书号
+    char book_name[20];                //书名
+    char book_at_name[30];            //作者名
+    char book_cp_name[30];            //出版社
+    float book_price;                //价格
+    int book_rest;                    //库存
+    struct book *next;                //下本书
+} Book;
+
+typedef struct stu {
+    char stu_acc[10];                //学生账号
+    char stu_passw[15];                //学生密码
+    char stu_find_question[30];        //密保问题
+    char stu_find_passw[15];        //密保问题密码
+    char stu_num[10];                //学生学号
+    char stu_name[20];                //学生姓名
+    char stu_tel[15];                //学生电话
+    char stu_bor_book[10][20];        //所借书号
+    struct stu *next;                //下位学生
+} Student;
+
+typedef struct manager {
+    char man_name[15];                //管理员姓名
+    char man_acc[10];                //管理员账号
+    char man_passw[15];                //管理员密码
+    struct manager *next;            //下个管理员
+} Manager;
+
+
+extern Book *Book_head;
+extern Student *Student_head;
+extern Manager *Manager_head;
+
+
+extern Book *Book_load();
+
+extern Student *Student_load(void);
+
+extern Manager *Manager_load(void);
+
+extern void Save_Book(void);
+
+extern void Save_Student(void);
+
+extern void Save_Manger(void);
+
+extern void Massage_Save(void);
+
 #endif //SIMPLE_BOOK_MANAGEMENT_SYSTEM_DATAMANAGEMENT_H
