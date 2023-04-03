d4178b77881f3c7a7d170dde255e048232f8f701
amibas <towsod@outlook.com>
2023-04-03 18:02:30 +0800

    改变了编号的数据类型

--- DataManagement.h
+++ DataManagement.h
@@ -6,8 +6,15 @@
 #define SIMPLE_BOOK_MANAGEMENT_SYSTEM_DATAMANAGEMENT_H
 
 
+#define USER_FILE "user.txt"
+#define BOOK_FILE "book.txt"
+#define NOTICE_FILE "notice.txt"
+
+#include "FileTools.h"
+#include "stdlib.h"
+
 typedef struct bookInfo {
-    char bookId[8];         //编号
+    int bookId;             //编号
     char bookName[20];      //书名
     char author[10];        //作者
     int total;              //数量
@@ -15,7 +22,7 @@ typedef struct bookInfo {
 } BOOK;
 
 typedef struct userInfo {
-    char userId[8];         //用户ID
+    int userId;             //用户ID
     char userName[10];      //用户名
     char userPassword[16];  //用户密码
     char sex;               //性别
@@ -24,12 +31,16 @@ typedef struct userInfo {
     struct userInfo *next;  //链表链接
 } USER;
 
-typedef struct notice{
-    char noticeId[8];       //公告ID
+typedef struct notice {
+    int noticeId;           //公告ID
     char noticeTitle[20];   //公告标题
     char text[400];         //正文
     char noticeTime[11];    //发布时间
     struct notice *next;    //链表链接
-}NOTICE;
+} NOTICE;
 
+
+extern int addUser(USER *user);
+extern int findUser(char *userName);    //查找用户
+extern int userPasswordCheck(char *userName, char *userPassword);    //密码检查
 #endif //SIMPLE_BOOK_MANAGEMENT_SYSTEM_DATAMANAGEMENT_H
