bd23ad5d7255fff3f2365c86ce5c3cdb72e6e134
amibas <towsod@outlook.com>
2023-04-03 20:51:57 +0800

    不知道放哪里的函数

---
+++ NotClassified.h
@@ -0,0 +1,25 @@
+//
+// Created by amiba on 2023/4/3.
+//
+
+#ifndef SIMPLE_BOOK_MANAGEMENT_SYSTEM_NOTCLASSIFIED_H
+#define SIMPLE_BOOK_MANAGEMENT_SYSTEM_NOTCLASSIFIED_H
+
+
+#include <stdlib.h>
+#include <time.h>
+#include <conio.h>
+#include <stdio.h>
+#include <string.h>
+
+
+extern void RandStr(int l, char *ch);
+
+extern void Password_Input(char *Password);
+
+extern int Password_Text(char *Password);
+
+extern int Password_Text_Find(char *Password);
+
+
+#endif //SIMPLE_BOOK_MANAGEMENT_SYSTEM_NOTCLASSIFIED_H
