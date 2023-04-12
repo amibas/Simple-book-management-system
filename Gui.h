71573bb6fa8e3e35c14d3ee2882516f431a11547
amibas <towsod@outlook.com>
2023-04-12 20:52:42 +0800

    补了些注释

--- Gui.h
+++ Gui.h
@@ -11,16 +11,16 @@
 #include<windows.h>
 
 
-extern void show_main(void);    //主界面
+extern void show_main(void);            //主界面
 
-extern void show_stu(void);     //学生端
+extern void show_stu(void);             //学生端
 
-extern void show_stu_enter(void);   //学生页
+extern void show_stu_enter(void);       //学生页
 
-extern void show_manager(void);     //管理端
+extern void show_manager(void);         //管理端
 
 extern void show_manager_enter(void);   //管理页
 
-extern void show_notice(void);      //公告端
+extern void show_notice(void);          //公告端
 
 #endif //SIMPLE_BOOK_MANAGEMENT_SYSTEM_GUI_H
--- ManagePage.h
+++ ManagePage.h
@@ -12,41 +12,41 @@
 #include "StudentPage.h"
 
 
-extern void Manager_Port(void);
+extern void Manager_Port(void);                         //管理端
 
-extern void Manager_Login(void);
+extern void Manager_Login(void);                        //管理端登录
 
-extern Manager *Manager_check(char *Manager_num);
+extern Manager *Manager_check(char *Manager_num);       //管理端登录检查
 
-extern void Manager_Look_Book(void);
+extern void Manager_Look_Book(void);                    //管理端查看书籍
 
-extern void Lookofnum(void);
+extern void Lookofnum(void);                            //编号升序查看书架
 
-extern void Lookofrest(void);
+extern void Lookofrest(void);                           //剩余升序查看书架
 
-extern void Lookofprice(void);
+extern void Lookofprice(void);                          //价格升序查看书架
 
-extern void Manager_Print_Book(void);
+extern void Manager_Print_Book(void);                   //管理端打印书籍
 
-extern void Manager_Change_Password(Manager *Man_num);
+extern void Manager_Change_Password(Manager *Man_num);  //管理端修改密码
 
-extern void Manager_Add_Book(void);
+extern void Manager_Add_Book(void);                     //管理端添加书籍
 
-extern void Manager_Add_Book1(void);
+extern void Manager_Add_Book1(void);                    //管理端添加书籍
 
-extern void Manager_Add_Book2(void);
+extern void Manager_Add_Book2(void);                    //管理端添加书籍
 
-extern void Manager_Down_Book(void);
+extern void Manager_Down_Book(void);                    //管理端下架书籍
 
-extern void Manager_Manager_Stu(void);
+extern void Manager_Manager_Stu(void);                  //管理端管理学生
 
-extern void Manager_Look_Stu(void);
+extern void Manager_Look_Stu(void);                     //管理端查看学生
 
-extern void Manager_Return_Stu(void);
+extern void Manager_Return_Stu(void);                   //管理端还书
 
-extern Student *Find_Stu(char *Stu_Stuty);
+extern Student *Find_Stu(char *Stu_Stuty);              //管理端查找学生
 
-extern void Manager_Del_Stu(void);
+extern void Manager_Del_Stu(void);                      //管理端删除学生
 
 
 #endif //SIMPLE_BOOK_MANAGEMENT_SYSTEM_MANAGEPAGE_H
